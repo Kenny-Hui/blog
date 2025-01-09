150685f36c53d5148bdf2fcf15bf2ec1908f6f52
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-01-10 00:07:01 +0800

    Fix broken RSS Feed

--- src/pages/feed/feed.xml.js
+++ src/pages/feed/feed.xml.js
@@ -1,10 +1,9 @@
 import rss from '@astrojs/rss';
-import sanitizeHtml from "sanitize-html";
 import { Constant } from '../../constant';
 import { getPostLink } from '../../components/PostCard.astro';
 
 export async function GET(context) {
-  const postFiles = import.meta.glob('../blog/**/*.{md,mdx}');
+  const postFiles = Object.values(import.meta.glob('../blog/**/*.{md,mdx}'));
   const posts = [];
 
   for(let postFile of Object.values(postFiles)) {
@@ -16,16 +15,18 @@ export async function GET(context) {
     title: Constant.brand,
     description: Constant.description,
     site: Constant.url,
-    items: posts.map(e => {
-      return {
-        title: e.frontmatter.title,
-        description: e.frontmatter.description,
-        link: Constant.url + getPostLink(e),
-        pubDate: e.frontmatter.pubDate,
-        author: Constant.author,
-        content: e.compiledContent ? e.compiledContent() : undefined // Can't render stuff in MDX
-      };
-    }),
+    items: await Promise.all(
+      posts.map(async e => {
+        return {
+          title: e.frontmatter.title,
+          description: e.frontmatter.description,
+          link: Constant.url + getPostLink(e),
+          pubDate: e.frontmatter.pubDate,
+          author: Constant.author,
+          content: e.compiledContent ? await e.compiledContent() : undefined // Can't render stuff in MDX
+        };
+      }),
+    ),
     customData: `<language>en-us</language>`,
   });
 }
